1bf4299ad2b1999853c50761fe5d33bb0f35cec4
Paulo Reis <pacmanreis@gmail.com>
2019-10-22 15:52:49 +0100

    Created burguer and controls

--- src/Components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/Components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import classes from './BurgerIngredient.css';
+import classes from './BurgerIngredient.module.css';
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
   render () {
     let ingredient = null;
-
-    switch (this.props.type) {
-      case ('bread-bottom'):
-        ingredient = <div className={classes.BurgerIngredient}></div>;
+    switch ( this.props.type ) {
+      case ( 'bread-bottom' ):
+        ingredient = <div className={classes.BreadBottom}></div>;
         break;
-      case ('bread-top'):
+      case ( 'bread-top' ):
         ingredient = (
         <div className={classes.BreadTop}>
           <div className={classes.Seeds1}></div>
@@ -18,23 +17,22 @@ class BurgerIngredient extends Component {
         </div>
         );
         break;
-      case ('meat'):
+      case ( 'meat' ):
         ingredient = <div className={classes.Meat}></div>;
         break;
-      case ('cheese'):
+      case ( 'cheese' ):
         ingredient = <div className={classes.Cheese}></div>;
         break;
-      case ('Bacon'):
+      case ( 'Bacon' ):
         ingredient = <div className={classes.Bacon}></div>;
         break;
-      case ('Salad'):
+      case ( 'Salad' ):
         ingredient = <div className={classes.Salad}></div>;
         break;
       default:
         ingredient = null;
-
-      return ingredient;
     }
+    return ingredient;
   }
 }
 
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Burger from '../../Components/Burger/Burger'
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
+state = {
+  ingredients: {
+    Salad: 0,
+    Bacon: 0,
+    cheese: 1,
+    meat: 0
+  }
+}
+
   render () {
     return <>
-      <div>Burger</div>
-      <div>Build Controls</div>
+      <Burger ingredients={this.state.ingredients} />
+      <BuildControls />
     </>;
   }
 }
