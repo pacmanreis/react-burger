862b65deffd138dfa37346e772a71050710008ed
Paulo Reis <pacmanreis@gmail.com>
2019-10-23 10:27:52 +0100

    Buttons now update state

--- src/Components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/Components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -23,10 +23,10 @@ class BurgerIngredient extends Component {
       case ( 'cheese' ):
         ingredient = <div className={classes.Cheese}></div>;
         break;
-      case ( 'Bacon' ):
+      case ( 'bacon' ):
         ingredient = <div className={classes.Bacon}></div>;
         break;
-      case ( 'Salad' ):
+      case ( 'salad' ):
         ingredient = <div className={classes.Salad}></div>;
         break;
       default:
